ef6687ec75ada395cd8320c7d4b8a4336b2d2ed9
heckadecimal <heckadec@gmail.com>
2026-07-09 00:56:10 -0400

    file sys js prototype idk

--- js/file-sys.js
+++ js/file-sys.js
@@ -19,18 +19,30 @@ let bracket = document.createElement("span");
 bracket.innerHTML = "├─ ";
 bracket.className = "bracket";
 
+let pipe = document.createElement("span");
+pipe.innerHTML = "│";
+pipe.className = "pipe";
+
 let folders = document.querySelectorAll(".folder");
 
 let files = document.querySelectorAll(".file");
 
 folders.forEach(function(folder){
     folder.prepend(folderIcon.cloneNode(true));
+
+    if (folder !== folder.parentElement.lastElementChild){
+        if (folder !== folder.parentElement.firstElementChild){
+            folder.prepend(bracket.cloneNode(true));
+        }
+    } else {
+        folder.prepend(endBracket.cloneNode(true));
+    }
 });
 
 files.forEach(function(file){
     file.prepend(fileIcon.cloneNode(true));
 
-    if (file.nextSibling != null){
+    if (file !== file.parentElement.lastElementChild){
         file.prepend(bracket.cloneNode(true));
     } else {
         file.prepend(endBracket.cloneNode(true));
